367c1982d8b1c023f79e710e8ddb9c61c6a9b3fb
Nguyễn Đức Hải <110242130+DucHai972@users.noreply.github.com>
2023-05-09 21:46:33 +0700

    Add select color window

--- detect_object.py
+++ detect_object.py
@@ -1,13 +1,63 @@
 import cv2
-
 from ulti import get_limits
+import tkinter as tk
+from tkinter import messagebox
+
+color_map = {
+    "yellow": [0, 255, 255],
+    "green": [0, 255, 0],
+    "blue": [255, 0, 0],
+    "orange": [0, 165, 255],
+    "purple": [127, 0, 127]
+}
+
+###################################
+# User select color
+def select_color():
+    color_choice = color_var.get()
+    if color_choice not in ['yellow', 'green', 'blue', 'orange', 'purple']:
+        messagebox.showerror("Error", "Invalid color choice!")
+        return
+    root.destroy()
+
+#Initiate window
+root = tk.Tk()
+root.geometry("400x350")
+root.title("Select color")
+
+color_var = tk.StringVar(value="yellow")
+color_frame = tk.Frame(root, bd=2, relief="groove", padx=10, pady=10)
+color_frame.pack()
 
 
-yellow = [0, 255, 255] # yellow in BRG color space
-green = [0, 255, 0] # green in BRG color space
-blue = [255, 0 , 0] # blue in BRG color space
-orange = [0, 165, 255] # orange in BRG color space
-purple = [127, 0, 127] # purple in BRG color space
+tk.Label(color_frame, text="Select color:").pack()
+
+options_frame = tk.Frame(color_frame)
+options_frame.pack()
+
+yellow_button = tk.Radiobutton(color_frame, text="Yellow", variable=color_var, value="yellow",
+                               fg="black", bg="#F7DC6F", font=("Arial", 12))
+green_button = tk.Radiobutton(color_frame, text="Green", variable=color_var, value="green",
+                              fg="black", bg="#2ECC71", font=("Arial", 12))
+blue_button = tk.Radiobutton(color_frame, text="Blue", variable=color_var, value="blue",
+                             fg="black", bg="#3498DB", font=("Arial", 12))
+orange_button = tk.Radiobutton(color_frame, text="Orange", variable=color_var, value="orange",
+                               fg="black", bg="#F39C12", font=("Arial", 12))
+purple_button = tk.Radiobutton(color_frame, text="Purple", variable=color_var, value="purple",
+                               fg="black", bg="#9B59B6", font=("Arial", 12))
+
+yellow_button.pack(anchor="w", pady=5)
+green_button.pack(anchor="w", pady=5)
+blue_button.pack(anchor="w", pady=5)
+orange_button.pack(anchor="w", pady=5)
+purple_button.pack(anchor="w", pady=5)
+
+select_button = tk.Button(root, text="Select", command=select_color, pady=10)
+select_button.pack()
+
+root.mainloop()
+####################################
+
 
 # Create a VideoCapture object to capture video from the default camera (0)
 cap = cv2.VideoCapture(0)
@@ -29,26 +79,24 @@ while cap.isOpened():
     if not ret:
         print("Error reading video stream")
         break
-    
+
     # Convert the color of frame to HSV format
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
+
     # Blur the image using Gaussian blur
     blur = cv2.GaussianBlur(hsvImage, (5, 5), 0)
-    
-    #get limit of any color that i listed above
-    lowerLimit, upperLimit = get_limits(color=yellow) # change here
+
+    # get limit of yellow color
+    lowerLimit, upperLimit = get_limits(color=color_map[color_var.get()])
     mask = cv2.inRange(blur, lowerLimit, upperLimit)
-        
- 
+
     # Perform morphological opening to remove noise
     mask = cv2.erode(mask, kernel, iterations=2)
     mask = cv2.dilate(mask, kernel, iterations=2)
-    
- 
+
     # Find the contours of the object in the mask
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
+
     # Filter the contours based on size and aspect ratio
     for cnt in contours:
         area = cv2.contourArea(cnt)
@@ -56,18 +104,15 @@ while cap.isOpened():
             continue
         approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
 
-        # Get the coordinates of the object
         x, y, w, h = cv2.boundingRect(approx)
-        
-        #crop the object to display
-        cropped = frame[y:y+h+10, x:x+w+10]
+
+        cropped = frame[y:y + h + 10, x:x + w + 10]
         if cropped is not None:
-            cv2.imshow("cropped", cropped)     
+            cv2.imshow("cropped", cropped)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
 
-       
-        
-    # Display the resulting frame, mask
+    # Display the resulting frame
+
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
     # Exit the loop if 'q' is pressed
